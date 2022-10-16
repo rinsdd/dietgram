7fb8b30de3efd18c3affae25f71010806c468383
hoirin.uxon <rinskyhs@gmail.com>
2022-10-16 17:40:39 +0000

    posts

--- app/User.php
+++ app/User.php
@@ -36,4 +36,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    public function records()
+    {
+        return $this->hasMany(Record::class);
+    }
+    
+    public function loadRelationshipCounts()
+    {
+        $this->loadCount('records');
+    }
 }
---
+++ database/migrations/2022_10_17_013621_create_records_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('content');
+            //$table->string('image'); //image
+            $table->timestamps();
+            
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+}
